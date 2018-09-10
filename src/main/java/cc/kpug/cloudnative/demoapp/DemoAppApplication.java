06389255b3cfa9e30d2624f07d267ca2dc8c184c
Joseph Yang <before30@gmail.com>
2018-09-11 00:56:58 +0900

    Add init

---
+++ src/main/java/cc/kpug/cloudnative/demoapp/DemoAppApplication.java
@@ -0,0 +1,12 @@
+package cc.kpug.cloudnative.demoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoAppApplication.class, args);
+    }
+}
